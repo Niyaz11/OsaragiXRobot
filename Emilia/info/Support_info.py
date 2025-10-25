458115ebe8729445678b9191faa09cbf7e427f04
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-10-25 23:46:39 +0530

    Update and rename clone_info.py to Support_info.py

---
+++ Emilia/info/Support_info.py
@@ -0,0 +1,15 @@
+__mod_name__ = "Support"
+
+__help__ = """
+• /broadcast `-flag <reply>`: Broadcasts the replied message to all the groups where the cloned bot is present.
+
+Flags available for broadcast:
+- `-all`: Broadcast to all groups and users.
+- `-chats`: Broadcast to all groups only.
+- `-users`: Broadcast to all users only.
+
+Example:
+- `/broadcast -all <reply to message>`
+
+PS: If you need any help, feel free to ask in our support [here.](https://t.me/EternalsHelplineBot) I have created this feature with my heart, and it took a lot of time and effort. So, please don't hesitate to /donate to keep this feature alive.
+"""
