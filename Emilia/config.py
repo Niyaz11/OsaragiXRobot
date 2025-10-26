68bcc3cd3b14abf49156f31db045fa3516d4824c
Niyaz11 <145466262+Niyaz11@users.noreply.github.com>
2025-10-26 15:23:18 +0530

    Update config.py

--- Emilia/config.py
+++ Emilia/config.py
@@ -14,13 +14,13 @@ class Config(object):
     BOT_ID = 8015912062 # BOT_ID
     BOT_USERNAME = "OsaragiXRobot" # BOT_USERNAME
 
-    MONGO_DB_URL = "" # MongoDB URL from MongoDB Atlas
+    MONGO_DB_URL = "mongodb+srv://osaragixrobot:fCW12lWsOiwgqSfw@cluster0.lddzvly.mongodb.net/" # MongoDB URL from MongoDB Atlas
 
     SUPPORT_CHAT = "EternalsHelplineBot" # Support Chat Username
     UPDATE_CHANNEL = "AnimeNexusNetwork" # Update Channel Username
     START_PIC = "https://i.ibb.co/DgpFbXsW/tmpfgm5zme3.jpg" # Start Image
     DEV_USERS = [7654385403,7852686677] # Dev Users
-    TOKEN = "" # Bot Token from @BotFather
+    TOKEN = "8015912062:AAEa_n5sadzLA-dgbkaRcbf5uQ77Cut4kVY" # Bot Token from @BotFather
     CLONE_LIMIT = 0 # Number of clones your bot can make
 
     EVENT_LOGS = -1002456565415 # Event Logs Chat ID
