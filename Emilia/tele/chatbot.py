2441dd17e13ecf75e2db9a0c1a9af69f0cee71df
Niyaz11 <145466262+Niyaz11@users.noreply.github.com>
2025-10-26 15:18:51 +0530

    Update chatbot.py

--- Emilia/tele/chatbot.py
+++ Emilia/tele/chatbot.py
@@ -17,7 +17,7 @@ from Emilia.custom_filter import register
 from Emilia.functions.admins import is_admin
 from Emilia.utils.decorators import *
 
-API_KEY = ("AIzaSyC5jmCIHT6v7eu3P_d0UFgvsaA6iBupgkQ") # Get your API key from Google Gemini API
+API_KEY = os.getenv("GEMINI_API_KEY", "AIzaSyDzBcTJ51yXZgzlEI9m5llmtFOzNjlS-MI") # Get your API key from Google Gemini API
 client = genai.Client(api_key=API_KEY)
 chatbotdb = db.chatbotto
 convodb = db.gemini_convos
