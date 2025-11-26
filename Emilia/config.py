0573aa6dc8c3b90292fa9e498775709171e50976
Niyaz11 <145466262+Niyaz11@users.noreply.github.com>
2025-11-27 03:28:51 +0530

    Update config.py

--- Emilia/config.py
+++ Emilia/config.py
@@ -20,7 +20,7 @@ class Config(object):
     UPDATE_CHANNEL = "AnimeNexusNetwork" # Update Channel Username
     START_PIC = "https://i.ibb.co/DgpFbXsW/tmpfgm5zme3.jpg" # Start Image
     DEV_USERS = [7654385403,7852686677] # Dev Users
-    TOKEN = "7845096754:AAFCJwDU3o_tgiypuQmhFyc_YyoQ4s1yRr0" # Bot Token from @BotFather
+    TOKEN = "8462016049:AAFRuEfssPXbmMvjTBD2ztNcOHIzwpB-pgQ" # Bot Token from @BotFather
     CLONE_LIMIT = 1 # Number of clones your bot can make
 
     EVENT_LOGS = -1002456565415 # Event Logs Chat ID
