14dbd751d6e463cad5a14a390be6eaf5816c151c
Niyaz11 <145466262+Niyaz11@users.noreply.github.com>
2025-11-27 02:04:29 +0530

    Update broadcast.py

--- Emilia/tele/broadcast.py
+++ Emilia/tele/broadcast.py
@@ -1,4 +1,4 @@
- from pyrogram import filters
+from pyrogram import filters
 from pyrogram.errors import FloodWait, UserIsBlocked, PeerIdInvalid
 from Emilia import pgram, LOGGER
 from Emilia.data import HELPABLE
